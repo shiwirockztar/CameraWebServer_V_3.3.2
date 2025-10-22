5914beabf45a5ca32631b9fb9cdd3a88484afd8e
shiwirockztar <shiwirockztar@gmail.com>
2025-10-22 13:07:45 -0500

    add credentials
    Add ssid and pasword

--- CameraWebServer_v_3_3_2.ino
+++ CameraWebServer_v_3_3_2.ino
@@ -9,8 +9,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char *ssid = "**********";
-const char *password = "**********";
+const char *ssid = "ALICIA ISABEL";
+const char *password = "39014972";
 
 void startCameraServer();
 void setupLedFlash();
